07396596f327f59f298fdc91b24e26fe05507bd1
hexacry <hexacry@gmail.com>
2022-04-28 08:15:11 -0500

    [Update] add cors and disable X-Powered-By

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require('express');
 const path = require('path');
 const router = require('./routes/router')
 
-const port = process.env.PORT | 9090
+const port = process.env.PORT || 9090
 
 const app = express();
 
+// Frontend
 app.use(express.static(path.join(__dirname, 'public')));
+// Middlewares
 app.use(express.json());
-app.use('/api', router)
+app.use(require('cors')());
+// Routes
+app.use('/api', router);
+// Config
+app.disable('x-powered-by');
 
 app.listen(port, () => {
   console.log(`Listen on port ${port}`)
